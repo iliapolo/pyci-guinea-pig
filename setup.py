b22803b93eaca693db78f9d551ec295946765135
iliapolo <eli.polonsky@gmail.com>
2018-04-17 19:12:09 +0300

    Added necessary files for a standard python project

---
+++ setup.py
@@ -0,0 +1,34 @@
+#############################################################################
+# Copyright (c) 2018 Eli Polonsky. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   * See the License for the specific language governing permissions and
+#   * limitations under the License.
+#
+#############################################################################
+
+
+from setuptools import setup
+
+
+BASE_PACKAGE_NAME = 'pyci_guinea_pig'
+
+PROGRAM_NAME = 'pyci-guinea-pig'
+
+PROJECT_NAME = 'pyci-guinea-pig'
+
+setup(
+    name=PROJECT_NAME,
+    version='0.0.1',
+    author='Eli Polonsky',
+    author_email='eli.polonsky@gmail.com',
+    license='LICENSE',
+)
